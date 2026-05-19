25c424b7ebf8f899d31fb03aa09547e77da11149
antor2005 <antor2005@yandex.ru>
2026-05-19 18:45:44 +0300

    requests to backend

--- burn-calc/src/api/Axios.ts
+++ burn-calc/src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: '/',
+  baseURL: 'http://localhost:8000/',
   withCredentials: true, // для отправки/приёма cookie с sessionId
   headers: {
     'Content-Type': 'application/json',
--- burn-calc/src/api/index.ts
+++ burn-calc/src/api/index.ts
@@ -1,6 +1,6 @@
 import { Api } from './Api';
 
 export const apiClient = new Api({
-    baseURL: '/',
+    baseURL: 'http://localhost:8000/',
     withCredentials: true, // Важно для отправки cookie с sessionId
 });
